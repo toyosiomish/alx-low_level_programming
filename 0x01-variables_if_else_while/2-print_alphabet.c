876bd372cdb2c5c713376a480a24f775f7e475d9
toyosiomish <toyosiomi@outlook.com>
2023-07-15 13:46:25 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return
+ */
+int main(void)
+{
+char c;
+c = 'a';
+while (c <= 'z')
+{
+putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
+}
