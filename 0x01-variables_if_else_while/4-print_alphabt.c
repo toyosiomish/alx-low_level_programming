5b87ff131321ee3f5306d6b1576f1bb6ebbadc57
toyosiomish <toyosiomi@outlook.com>
2023-07-15 14:39:32 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+char letter = 'a';
+while (letter <= 'z')
+{
+if (letter != 'q' && letter != 'e')
+putchar(letter);
+letter ++;
+}
+putchar('\n');
+return (0);
+}
