f2e8d02b55b3779feeb6df41748e9a48629ea09f
toyosiomish <toyosiomi@outlook.com>
2023-07-15 14:54:25 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return 0 (Success)
+ */
+int main(void)
+{
+int num1 = 0;
+while (num1 <= 9)
+{
+putchar(num1 + '0');
+num1++;
+}
+putchar('\n');
+return (0);
+}
+
