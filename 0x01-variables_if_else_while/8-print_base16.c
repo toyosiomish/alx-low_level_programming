63442625c070b1776ecc64efa2976c01b5a1fc85
toyosiomish <toyosiomi@outlook.com>
2023-07-15 15:17:51 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int number;
+for (number = 0; number < 16; number++)
+{
+if (number < 10)
+putchar(number + '0');
+else 
+putchar(number - 10 + 'a');
+}
+putchar('\n');
+return (0);
+}
+
