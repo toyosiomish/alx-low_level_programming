dbd1ae5453a052fb9021d871aa074611a51b5443
toyosiomish <toyosiomi@outlook.com>
2023-07-15 12:44:13 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,7 +6,7 @@
  *
  * Return: 0 (Success)
  */
-int main (void)
+int main(void)
 {
 int n;
 srand(time(0));
@@ -15,7 +15,7 @@ if (n > 0)
 printf("%d is positive\n", n);
 else if (n < 0)
 printf("%d is negative\n", n);
-else 
+else
 printf("0 is zero\n");
 return (0);
 }				
