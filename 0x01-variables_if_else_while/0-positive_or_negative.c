945be56e989f653d4128af6b274de18b425c26c4
toyosiomish <toyosiomi@outlook.com>
2023-07-15 12:40:32 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,11 +11,11 @@ int main (void)
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-if (n > 0)
-printf ("%d is positive\n", n);
-else if (n < 0)
-printf ("%d is negative\n", n);
+if(n > 0)
+printf("%d is positive\n", n);
+else if(n < 0)
+printf("%d is negative\n", n);
 else 
-printf ("0 is zero\n");
-return (0);
+printf("0 is zero\n");
+return(0);
 }				
