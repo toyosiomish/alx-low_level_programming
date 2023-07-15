5959330e17ecb64f2f09a9d3123eb97c9ba458f1
toyosiomish <toyosiomi@outlook.com>
2023-07-15 10:37:22 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,25 +13,21 @@ int main(void)
 
 	srand(time(0)); /* Initialize random number generator with current time */
 
-	n = rand(); /* Generate a random number and assign it to variable n */
-
-	printf("%d is ", n);
-
-	if(n > 0) 
-	{	
-     		printf("The number %d is positive", n);
-	}		
-	else if(n == 0)
-	{
-    		printf("The number %d is zero", n);
-	}	
-	else
-	{ 
-	    	printf("The number %d is negative", n);
-	}
-
-	printf("\n");
-
-	return (0);
+n = rand(); /* Generate a random number and assign it to variable n */
+printf("%d is ", n);
+if(n > 0) 
+{	
+printf("%d is positive", n);
+}		
+else if(n == 0)
+{    		
+printf("%d is zero", n);
+}	
+else
+{ 
+printf("%d is negative", n);
+}
+printf("\n");
+return (0);
 }
 
