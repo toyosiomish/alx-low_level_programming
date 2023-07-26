a52940429f682909156fdb13fd9a552bd96c7113
toyosiomish <toyosiomi@outlook.com>
2023-07-26 12:03:13 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -16,12 +16,6 @@ p = &n;
    * - only one statement
    * - you are not allowed to code anything else than this line of code
    */
-printf("n = %p\n", &n);
-printf("a[0] = %p\n", &a[0]);
-printf("a[1] = %p\n", &a[1]);
-printf("a[2] = %p\n", &a[2]);
-printf("p = %p\n", (p + 5));
-printf("a[3] = %p\n", &a[3]);
 *(p + 5) = 98;
   /* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
